ea06f38921491a7419d4ef63da237b3df0541413
shenfeng1945 <823902826@qq.com>
2017-08-07 21:24:56 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## viewport
 ## A simple page
+## jQuery库、Flex布局、媒体查询、轮播 
 ## 预览[https://shenfeng1945.github.io/viewport](https://shenfeng1945.github.io/viewport)
--- loadMore.js
+++ loadMore.js
@@ -5,12 +5,10 @@ button.onclick = function () {
     let request = new XMLHttpRequest()
     request.open('GET', `./JSON/page-${n}.html`)
     request.onload = function () {
-        n +=1
+        n += 1
         let response = request.responseText
         let data = window.JSON.parse(response)
-        console.log(data)
-        console.log(response)
-        for (var i = 0; i<data.content.length; i++) {
+        for (var i = 0; i < data.content.length; i++) {
             let liList = `
         <li><img src=${data.content[i].url}><h3>${data.content[i].h3}</h3><p>${data.content[i].p}</p></li>
        `
@@ -21,12 +19,12 @@ button.onclick = function () {
             button.textContent = '没有更多了'
         }
     }
-    request.send()
+        request.send()
 }
-window.onscroll = function(){
+window.onscroll = function () {
     let buttonTop = button.getBoundingClientRect().top
     let viewHeight = document.documentElement.clientHeight
-    if(viewHeight > buttonTop + 60){
+    if (viewHeight > buttonTop + 60) {
         button.click()
     }
 }
