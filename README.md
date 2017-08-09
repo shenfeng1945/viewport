784aae9a0b0909dab62c9b73e242044a69275f5a
shenfeng1945 <823902826@qq.com>
2017-08-09 15:58:51 +0800

    优化加载

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## A simple page
-## jQuery库、Flex布局、媒体查询、轮播 
-## 预览[https://shenfeng1945.github.io/viewport](https://shenfeng1945.github.io/viewport)
+## 海贼王人设页面
+#### jQuery库、Flex布局、媒体查询、轮播 
+#### 预览[https://shenfeng1945.github.io/viewport](https://shenfeng1945.github.io/viewport)
+#### 结合自动加载、懒加载和预加载
--- loadMore.js
+++ loadMore.js
@@ -1,30 +1,63 @@
+//预加载,让一定会显示的图片提前下载，例如菊花图
+// let loadingImage = new Image()
+// loadingImage.src = "https://media.giphy.com/media/y1ZBcOGOOtlpC/giphy.gif"
 let button = document.querySelector('.button')
 let olInsert = document.querySelector('.introduce > ol')
 let n = 1
-button.onclick = function () {
+//标记作用，判断插入的data(JSON)是否为最后一张。
+let hasNext = true
+//标记作用，在一个请求未成功之前，不在发起另外一个请求。
+let loadingImage = false
+button.onclick = loadMore
+ function loadMore() {
+     if(! hasNext){return}
+     if(loadingImage){return}
     let request = new XMLHttpRequest()
     request.open('GET', `./JSON/page-${n}.html`)
+    //图片加载失败后。也会进行下一次请求。
+    request.onerror = function(){loadingImage = false}
     request.onload = function () {
+        loadingImage = false
         n += 1
         let response = request.responseText
         let data = window.JSON.parse(response)
         for (var i = 0; i < data.content.length; i++) {
             let liList = `
-        <li><img src=${data.content[i].url}><h3>${data.content[i].h3}</h3><p>${data.content[i].p}</p></li>
+        <li><img src="https://media.giphy.com/media/y1ZBcOGOOtlpC/giphy.gif" data-src=${data.content[i].url}><h3>${data.content[i].h3}</h3><p>${data.content[i].p}</p></li>
        `
             olInsert.insertAdjacentHTML('beforeend', liList)
         }
         if (data.hasNext === false) {
-            button.disabled = true
+            hasNext = false
+            // button.disabled = true
             button.textContent = '没有更多了'
         }
     }
-        request.send()
+    loadingImage = true
+    request.send()
 }
 window.onscroll = function () {
-    let buttonTop = button.getBoundingClientRect().top
+   if(toUp(button)){
+       loadMore()
+   }
+    let images = document.querySelectorAll('img[data-src]')
+    for(let i=0;i<images.length;i++){
+        if(toUp(images[i])){
+            //不能直接用images[i].data-src赋值。
+            images[i].src = images[i].getAttribute('data-src')
+            //图片加载成功后，删除属性data-src，使下一次的images少一张。起着优化作用。
+            images[i].removeAttribute('data-src')
+        }
+    }
+ }
+//判断一个element是否出现在页面里。(element从下到上)
+function toUp(element){
+   let buttonTop = element.getBoundingClientRect().top
     let viewHeight = document.documentElement.clientHeight
     if (viewHeight > buttonTop + 60) {
-        button.click()
+        return true
+    }else{
+        return false
     }
+
 }
